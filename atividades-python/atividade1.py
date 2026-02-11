dc6ceb491dc76b32958d472f798b47c9310584bd
davibasilva-star <davi_ba_silva@estudante.sesisenai.org.br>
2026-02-11 14:50:31 +0000

    update

---
+++ atividades-python/atividade1.py
@@ -0,0 +1,6 @@
+#primeira atividade de python usando a concatenação de valores
+print("ola mundo")
+numero1 = input("Digite o numero1: ")
+numero2 = input("Digite o numero2: ")
+soma = numero1+numero2
+print("a soma do numro 1 e numero 2 é: ",soma)
---
+++ atividades-python/atividade2.py
@@ -0,0 +1,12 @@
+#Atividade somando valores flutuantes
+numero1 = float (input("insira o primeiro numero: "))
+numero2 = float (input("insira o segundo numero: "))
+soma = numero1+numero2
+print("A  soma dos dois numeros é: ", soma)
+#Atividade media de notas de aluno.
+nota1 = float(input("insira a primeira nota"))
+nota2= float(input("insira a segunda nota"))
+nota3 = float(input("insira a terceira nota"))
+nota4 = float(input("insira a quarta nota"))
+media = (nota1+nota2+nota3+nota4) /4
+print("A media das notas do aluno é: ",media)
